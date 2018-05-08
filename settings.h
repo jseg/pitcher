4e16fab7b035dc08f762f2cdcb05df47a5af1ee0
dafido <dafidowen@gmail.com>
2018-05-08 16:43:03 -0700

    new text

--- settings.h
+++ settings.h
@@ -77,28 +77,28 @@ int defaultPresets[NUM_PRESETS][NUM_MOTORS] =
    {10, 70, 150 }   //9
  };
 
-const char rc1[] PROGMEM = "High and Away";
-const char rc2[] PROGMEM = "High";
-const char rc3[] PROGMEM = "High and Inside";
-const char rc4[] PROGMEM = "Outside";
-const char rc5[] PROGMEM = "Down the Middle";
-const char rc6[] PROGMEM = "Inside";
-const char rc7[] PROGMEM = "Low and Away ";
-const char rc8[] PROGMEM = "Low";
-const char rc9[] PROGMEM = "Low and Inside";
+const char rc1[] PROGMEM = "High Outside";
+const char rc2[] PROGMEM = "High Middle";
+const char rc3[] PROGMEM = "High Inside";
+const char rc4[] PROGMEM = "Middle Outside";
+const char rc5[] PROGMEM = "Middle Middle";
+const char rc6[] PROGMEM = "Middle Inside";
+const char rc7[] PROGMEM = "Low Outside ";
+const char rc8[] PROGMEM = "Low Middle";
+const char rc9[] PROGMEM = "Low Inside";
 
 PGM_P const right_table[] PROGMEM = {
   rc1, rc2, rc3, rc4, rc5, rc6, rc7, rc8, rc9};
 
-const char lc1[] PROGMEM = "High and Inside";
-const char lc2[] PROGMEM = "High";
-const char lc3[] PROGMEM = "High and Away";
-const char lc4[] PROGMEM = "Inside";
-const char lc5[] PROGMEM = "Down the Middle";
-const char lc6[] PROGMEM = "Outside";
-const char lc7[] PROGMEM = "Low and Inside ";
-const char lc8[] PROGMEM = "Low";
-const char lc9[] PROGMEM = "Low and Away";
+const char lc1[] PROGMEM = "High Inside";
+const char lc2[] PROGMEM = "High Middle";
+const char lc3[] PROGMEM = "High Outside";
+const char lc4[] PROGMEM = "Middle Inside";
+const char lc5[] PROGMEM = "Middle Middle";
+const char lc6[] PROGMEM = "Middle Outside";
+const char lc7[] PROGMEM = "Low Inside ";
+const char lc8[] PROGMEM = "Low Middle";
+const char lc9[] PROGMEM = "Low Outside";
 
 PGM_P const left_table[] PROGMEM = {
   lc1, lc2, lc3, lc4, lc5, lc6, lc7, lc8, lc9};
